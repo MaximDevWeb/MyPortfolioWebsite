b4b5958356f080754b7729cfc6258af3dde3c5f5
Makkssimka <swoisait@gmail.com>
2023-01-16 13:26:52 +0300

    Правки

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h2>contact</h2>
+  <main class="contact-page">
+    <h1>&lt;My contact&gt;</h1>
+
+    <div class="contact-page__wrap">
+      <div class="contact-page__form"></div>
+      <div class="contact-page__code"></div>
+    </div>
+  </main>
 </template>
